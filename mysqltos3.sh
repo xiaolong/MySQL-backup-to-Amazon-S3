d5240a87343780087cafa684389315d5e1905d29
Xiaolong Cheng <xiaolong.ch@gmail.com>
2012-12-14 13:20:23 -0800

    Update mysqltos3.sh

--- mysqltos3.sh
+++ mysqltos3.sh
@@ -4,17 +4,17 @@
 # Under a MIT license
 
 # change these variables to what you need
-MYSQLROOT=root
-MYSQLPASS=password
-S3BUCKET=bucketname
-FILENAME=filename
-DATABASE='--all-databases'
+MYSQLROOT=rpacep
+MYSQLPASS=25fasiuz
+S3BUCKET=epochbackup
+
 # the following line prefixes the backups with the defined directory. it must be blank or end with a /
 S3PATH=mysql_backup/
+
 # when running via cron, the PATHs MIGHT be different. If you have a custom/manual MYSQL install, you should set this manually like MYSQLDUMPPATH=/usr/local/mysql/bin/
 MYSQLDUMPPATH=
 #tmp path.
-TMP_PATH=~/
+TMP_PATH=/home/rpacep/research/mysqlBackup/
 
 DATESTAMP=$(date +".%m.%d.%Y")
 DAY=$(date +"%d")
@@ -32,18 +32,19 @@ if [ ${PERIOD} = "auto" ]; then
 fi
 
 echo "Selected period: $PERIOD."
+echo "Starting backing up databases..."
 
-echo "Starting backing up the database to a file..."
+# dump databases
+${MYSQLDUMPPATH}mysqldump --single-transaction --user=${MYSQLROOT} --password=${MYSQLPASS} chinagames > ${TMP_PATH}chinagames_dump.sql
+${MYSQLDUMPPATH}mysqldump --single-transaction --user=${MYSQLROOT} --password=${MYSQLPASS} tracker > ${TMP_PATH}tracker_dump.sql
 
-# dump all databases
-${MYSQLDUMPPATH}mysqldump --quick --user=${MYSQLROOT} --password=${MYSQLPASS} ${DATABASE} > ${TMP_PATH}${FILENAME}.sql
-
-echo "Done backing up the database to a file."
+echo "Done backing up databases."
 echo "Starting compression..."
 
-tar czf ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz ${TMP_PATH}${FILENAME}.sql
+tar czf ${TMP_PATH}chinagames_dump${DATESTAMP}.tar.gz ${TMP_PATH}chinagames_dump.sql
+tar czf ${TMP_PATH}tracker_dump${DATESTAMP}.tar.gz ${TMP_PATH}tracker_dump.sql
 
-echo "Done compressing the backup file."
+echo "Done compressing the backup files."
 
 # we want at least two backups, two months, two weeks, and two days
 echo "Removing old backup (2 ${PERIOD}s ago)..."
@@ -56,12 +57,13 @@ echo "Past backup moved."
 
 # upload all databases
 echo "Uploading the new backup..."
-s3cmd put -f ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+s3cmd put -f ${TMP_PATH}chinagames_dump${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+s3cmd put -f ${TMP_PATH}tracker_dump${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+
 echo "New backup uploaded."
 
 echo "Removing the cache files..."
-# remove databases dump
-rm ${TMP_PATH}${FILENAME}.sql
-rm ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz
-echo "Files removed."
+rm ${TMP_PATH}$chinagames_dump${DATESTAMP}.tar.gz
+rm ${TMP_PATH}$tracker_dump${DATESTAMP}.tar.gz
+
 echo "All done."
